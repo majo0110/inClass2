5682eb724475d2f4f4acb35658c6a800d947b7df
Jonathan <majo@oregonstate.edu>
2021-04-29 16:36:00 -0700

    skelatal for division method and modified main

--- calculator.py
+++ calculator.py
@@ -1,9 +1,15 @@
 def main():
-    userChoice = input("Enter a number for functionality: ")
+    userChoice = input("Enter a number for calculator function: ")
 
 
-def addition(a, b):
+def addition(a,b):
     pass
 
-def subtraction(a, b):
+def subtraction(a,b):
     pass
+
+def multiplication(a,b):
+    pass
+
+if __name__ == '__main__':
+    main()
