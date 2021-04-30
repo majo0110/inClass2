62e685775dde9c7b3e442a1ab301edc001af64ca
Jonathan <majo@oregonstate.edu>
2021-04-30 16:00:10 -0700

    Finished division test module

--- test_calc.py
+++ test_calc.py
@@ -23,5 +23,17 @@ class testCase_Sub(unittest.TestCase):
     def test_sub_zero(self):
         self.assertEqual(calculator.subtraction(1000,0), 1000)
 
+class testCase_division(unittest.TestCase):
+    def test_division(self):
+        result = calculator.division(30,15)
+        self.assertEqual(result, 2)
+
+    def test_division_neg(self):
+        self.assertEqual( calculator.division(100, -40), -2.5)
+
+    def test_division_zeor(self):
+        self.assertRaises(ZeroDivisionError, calculator.division, 30,0)
+
+
 if __name__ == '__main__':
     unittest.main()
