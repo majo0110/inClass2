8b8e2811d79bdc5358a4cfdba90f5d42a4ffc3a7
Jonathan <majo@oregonstate.edu>
2021-04-30 15:46:19 -0700

    Added subtract test cases

--- test_calc.py
+++ test_calc.py
@@ -12,5 +12,16 @@ class testCase_Add(unittest.TestCase):
     def test_add_string(self):
         self.assertRaises(TypeError, calculator.addition('hello', 'world'))
 
+class testCase_Sub(unittest.TestCase):
+    def test_sub(self):
+        result = calculator.subtraction(10,15)
+        self.assertEqual(result, -5)
+
+    def test_sub_neg(self):
+        self.assertEqual(calculator.subtraction(15,-30), -15)
+
+    def test_sub_string(self):
+        self.assertRaises(TypeError, calculator.subtraction('hello', 'world'))
+
 if __name__ == '__main__':
     unittest.main()
