880afa6176a390487d13d0b8d45c2cebddde66eb
Jonathan <majo@oregonstate.edu>
2021-04-30 16:04:08 -0700

    Finished test module for multiplication and finished in class activity part 1

--- test_calc.py
+++ test_calc.py
@@ -23,6 +23,17 @@ class testCase_Sub(unittest.TestCase):
     def test_sub_zero(self):
         self.assertEqual(calculator.subtraction(1000,0), 1000)
 
+class testCase_mult(unittest.TestCase):
+    def test_mult(self):
+        result = calculator.multiplication(100,15)
+        self.assertEqual(result, 1500)
+
+    def test_mult_neg(self):
+        self.assertEqual(calculator.multiplication(45,-30), -1350)
+
+    def test_mult_zero(self):
+        self.assertEqual(calculator.multiplication(15000,0), 0)
+
 class testCase_division(unittest.TestCase):
     def test_division(self):
         result = calculator.division(30,15)
