ed4dc44a2c36cfdc8c1e75cab59a7e710e9afee6
Jonathan <majo@oregonstate.edu>
2021-04-30 15:44:44 -0700

    Added test cases for addition function

--- test_calc.py
+++ test_calc.py
@@ -1,5 +1,16 @@
 import unittest
 import calculator
 
+class testCase_Add(unittest.TestCase):
+    def test_add(self):
+        result = calculator.addition(10,10)
+        self.assertEqual(result, 20)
+
+    def test_add_neg(self):
+        self.assertEqual(calculator.addition(15,-30), -15)
+
+    def test_add_string(self):
+        self.assertRaises(TypeError, calculator.addition('hello', 'world'))
+
 if __name__ == '__main__':
     unittest.main()
